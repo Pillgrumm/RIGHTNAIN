1b02aae17198f916cd2132b65af1a5743809091a
Nicholas Valenzuela <>
2025-06-06 08:51:01 -0700

    JS

---
+++ script.js
@@ -0,0 +1,9 @@
+function showDay2() {
+  document.getElementById("day1").classList.remove("visible");
+  document.getElementById("day2").classList.add("visible");
+}
+
+function showDay1() {
+  document.getElementById("day2").classList.remove("visible");
+  document.getElementById("day1").classList.add("visible");
+}
